6d147f5212392626732c66a12865cab2ebfbf428
Stéphane Denis <sdenis@cegepsth.qc.ca>
2022-08-29 13:04:55 -0400

    en classe

--- graphe.pl
+++ graphe.pl
@@ -9,6 +9,11 @@ gr(f,c,3).
 gr(f,h,2).
 gr(c,d,3).
 gr(h,d,2).
-solution(A,B) :- gr(A,C,_), gr(C,B,_).
+solution(A,B) :- gr(A,B,_)
+    ; gr(A,Intermediaire,_)
+        , gr(Intermediaire,B,_) 
+    ; gr(A,Intermediaire1,_)
+        , gr(Intermediaire1,Intermediaire2,_)
+        , gr(Intermediaire2,B,_).
 distance(A,B,Segment1,Segment2) :- gr(A,C,Segment1), gr(C,B,Segment2).
 distance(A,B,Distance) :- distance(A,B,Segment1,Segment2), Distance is Segment1 + Segment2.
